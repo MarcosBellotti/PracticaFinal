d1a3424d200e712ca097c39e37170dbc5976f493
Marcos Bellotti <102260484+MarcosBellotti@users.noreply.github.com>
2024-03-13 17:07:47 -0300

    Update Principal.cs
    pregunta

--- Logica/Persistencia/Principal.cs
+++ Logica/Persistencia/Principal.cs
@@ -17,6 +17,7 @@ namespace Persistencia
         {
             get
             {
+                //ME PODES EXPLICAR QUE HICISTE ACA? xd
                 if (principal == null)
                 {
                     principal = new Principal();
